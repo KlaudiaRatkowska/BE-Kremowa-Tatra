dcb0be494d4e2b489c1f2297542535f585c05beb
Klaudia <klaudia.ratkowska.one@wp.pl>
2023-12-04 00:01:44 +0100

    src

---
+++ src/app/config/parameters.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'mysql',
+    'database_port' => '',
+    'database_name' => 'prestashop',
+    'database_user' => 'root',
+    'database_password' => 'prestashop',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'vmtrAScwL9XVGv5MzaROaahfgxkZr2bxOTP7kUzbd4GELqLeJqFkxbDuwwJZtTO7',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2023-11-17',
+    'locale' => 'pl-PL',
+    'use_debug_toolbar' => true,
+    'cookie_key' => '6tJgk5GRq8uRvtYQIIzey4OFLS4hvbrCtbSCeN7aOzP2RY4cqMecunrmAdWcjCDO',
+    'cookie_iv' => 'haQi3gZAvtDLIvWtQP8XcGXbIqVjQHSj',
+    'new_cookie_key' => 'def00000e764aef0898cb9bae3038210e20fc70fbbff6bef9a04e9a8b0bbf4876213631efc57e4f33b4a0bdea1ee90cc8c6af939d4e0fbf41cd7a7016fc54125c3689a26',
+  ),
+);
